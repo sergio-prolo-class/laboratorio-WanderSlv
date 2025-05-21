8a07fb83bc8013f04d2fed657f6d00b0dc917f8f
Wander Silva <165096619+WanderSlv@users.noreply.github.com>
2025-05-21 14:27:50 +0000

    Atividades 1 e 2

--- testeCommit.c
+++ testeCommit.c
@@ -2,7 +2,30 @@
 
 int main()
 {
-    printf("C ou não C?\n");    // teste
-    printf("Eis a questão!\n"); /*teste*/
-    return 0;
+    float altura, largura, profundidade;
+    float total, area, valor;
+    int qtd_caixa;
+
+    printf("Digite a Altrua (m): ");
+    scanf("%f", &altura);
+
+    printf("Digite a Largura (m): ");
+    scanf("%f", &largura);
+
+    printf("Digite a Profundidade (m): ");
+    scanf("%f", &profundidade);
+
+    area = 2 * (largura * altura) + 2 * (largura * profundidade) + 2 * (altura * profundidade);
+    total = altura * largura * profundidade;
+
+    printf("A área é: %f m² e o valor é o %f m³\n", area, total);
+
+    // att 2
+    printf("Digite a quantidade de caixas: ");
+    scanf("%d", &qtd_caixa);
+
+    valor = 0.5 * qtd_caixa;
+
+    printf("Valor total é: %f\n", valor);
+    return (0);
 }
